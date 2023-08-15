8281ff4e6994ec0dc8e9c589a40db4279ac59984
simrantan <96899862+simrantan@users.noreply.github.com>
2023-08-14 20:11:33 -0700

    Update get_features.py

--- model/framework/code/get_features.py
+++ model/framework/code/get_features.py
@@ -21,40 +21,35 @@ from sklearn.impute import SimpleImputer
 class FeaturesGeneration:
     def __init__(self):
         self.fingerprints = []
-
-    def select_descriptors(self, mol, selected_descriptor_names):
-        descriptor_names = [desc[0] for desc in Descriptors.descList]
-        selected_indices = [descriptor_names.index(name) for name in selected_descriptor_names]
-        descriptors = [Descriptors.descList[i][1](mol) for i in selected_indices]
-        return descriptors
-
     def get_fingerprints(self, df, model, fp_name, split, numpy_folder):
 
         smiles_list = df['SMILES_stand'].to_list()
         
         not_found = []
         selected_descriptors_rdkDes = ['MaxEStateIndex', 'MinEStateIndex', 'MaxAbsEStateIndex', 'MinAbsEStateIndex', 'qed', 'MolWt', 'HeavyAtomMolWt', 'ExactMolWt', 'NumValenceElectrons', 'NumRadicalElectrons', 'MaxPartialCharge', 'MinPartialCharge', 'MaxAbsPartialCharge', 'MinAbsPartialCharge', 'FpDensityMorgan1', 'FpDensityMorgan2', 'FpDensityMorgan3', 'BalabanJ', 'BertzCT', 'Chi0', 'Chi0n', 'Chi0v', 'Chi1', 'Chi1n', 'Chi1v', 'Chi2n', 'Chi2v', 'Chi3n', 'Chi3v', 'Chi4n', 'Chi4v', 'HallKierAlpha', 'Ipc', 'Kappa1', 'Kappa2', 'Kappa3', 'LabuteASA', 'PEOE_VSA1', 'PEOE_VSA10', 'PEOE_VSA11', 'PEOE_VSA12', 'PEOE_VSA13', 'PEOE_VSA14', 'PEOE_VSA2', 'PEOE_VSA3', 'PEOE_VSA4', 'PEOE_VSA5', 'PEOE_VSA6', 'PEOE_VSA7', 'PEOE_VSA8', 'PEOE_VSA9', 'SMR_VSA1', 'SMR_VSA10', 'SMR_VSA2', 'SMR_VSA3', 'SMR_VSA4', 'SMR_VSA5', 'SMR_VSA6', 'SMR_VSA7', 'SMR_VSA8', 'SMR_VSA9', 'SlogP_VSA1', 'SlogP_VSA10', 'SlogP_VSA11', 'SlogP_VSA12', 'SlogP_VSA2', 'SlogP_VSA3', 'SlogP_VSA4', 'SlogP_VSA5', 'SlogP_VSA6', 'SlogP_VSA7', 'SlogP_VSA8', 'SlogP_VSA9', 'TPSA', 'EState_VSA1', 'EState_VSA10', 'EState_VSA11', 'EState_VSA2', 'EState_VSA3', 'EState_VSA4', 'EState_VSA5', 'EState_VSA6', 'EState_VSA7', 'EState_VSA8', 'EState_VSA9', 'VSA_EState1', 'VSA_EState10', 'VSA_EState2', 'VSA_EState3', 'VSA_EState4', 'VSA_EState5', 'VSA_EState6', 'VSA_EState7', 'VSA_EState8', 'VSA_EState9', 'FractionCSP3', 'HeavyAtomCount', 'NHOHCount', 'NOCount', 'NumAliphaticCarbocycles', 'NumAliphaticHeterocycles', 'NumAliphaticRings', 'NumAromaticCarbocycles', 'NumAromaticHeterocycles', 'NumAromaticRings', 'NumHAcceptors', 'NumHDonors', 'NumHeteroatoms', 'NumRotatableBonds', 'NumSaturatedCarbocycles', 'NumSaturatedHeterocycles', 'NumSaturatedRings', 'RingCount', 'MolLogP', 'MolMR', 'fr_Al_COO', 'fr_Al_OH', 'fr_Al_OH_noTert', 'fr_ArN', 'fr_Ar_COO', 'fr_Ar_N', 'fr_Ar_NH', 'fr_Ar_OH', 'fr_COO', 'fr_COO2', 'fr_C_O', 'fr_C_O_noCOO', 'fr_C_S', 'fr_HOCCN', 'fr_Imine', 'fr_NH0', 'fr_NH1', 'fr_NH2', 'fr_N_O', 'fr_Ndealkylation1', 'fr_Ndealkylation2', 'fr_Nhpyrrole', 'fr_SH', 'fr_aldehyde', 'fr_alkyl_carbamate', 'fr_alkyl_halide', 'fr_allylic_oxid', 'fr_amide', 'fr_amidine', 'fr_aniline', 'fr_aryl_methyl', 'fr_azide', 'fr_azo', 'fr_barbitur', 'fr_benzene', 'fr_benzodiazepine', 'fr_bicyclic', 'fr_diazo', 'fr_dihydropyridine', 'fr_epoxide', 'fr_ester', 'fr_ether', 'fr_furan', 'fr_guanido', 'fr_halogen', 'fr_hdrzine', 'fr_hdrzone', 'fr_imidazole', 'fr_imide', 'fr_isocyan', 'fr_isothiocyan', 'fr_ketone', 'fr_ketone_Topliss', 'fr_lactam', 'fr_lactone', 'fr_methoxy', 'fr_morpholine', 'fr_nitrile', 'fr_nitro', 'fr_nitro_arom', 'fr_nitro_arom_nonortho', 'fr_nitroso', 'fr_oxazole', 'fr_oxime', 'fr_para_hydroxylation', 'fr_phenol', 'fr_phenol_noOrthoHbond', 'fr_phos_acid', 'fr_phos_ester', 'fr_piperdine', 'fr_piperzine', 'fr_priamide', 'fr_prisulfonamd', 'fr_pyridine', 'fr_quatN', 'fr_sulfide', 'fr_sulfonamd', 'fr_sulfone', 'fr_term_acetylene', 'fr_tetrazole', 'fr_thiazole', 'fr_thiocyan', 'fr_thiophene', 'fr_unbrch_alkane', 'fr_urea']
-
         for smi in smiles_list:
             try: 
                 m = Chem.MolFromSmiles(smi)
             
                 can_smi = Chem.MolToSmiles(m, True)
-                if fp_name == 'rdkDes':
-                    print("Selected SMILES:", can_smi)
-                    fp = self.select_descriptors(m, selected_descriptors_rdkDes)
-                    print("Descriptors:", fp) 
-                else:
-                    fp = fpFunc_dict[fp_name](m)
-                bit_array = np.asarray(fp)
+                
+                # Calculate only the specific 200 descriptors
+                descriptor_values = []
+                for descriptor_name in specific_descriptors:
+                    descriptor_func = getattr(rdMolDescriptors, descriptor_name)
+                    descriptor_value = descriptor_func(m)
+                    descriptor_values.append(descriptor_value)
+                
+                bit_array = np.asarray(descriptor_values)
                 self.fingerprints.append(bit_array)
             except:
-                not_found.append(smi)
+                # Handle exceptions and add placeholder values
+                self.fingerprints.append([np.nan] * len(specific_descriptors))
                 
                 if fp_name == 'tpatf':
                     add = [np.nan for i in range(self.fingerprints[0].shape[1])]
                 elif fp_name == 'rdkDes':
-                    add = [np.nan for i in range(len(selected_descriptors_rdkDes))]
+                    add = [np.nan for i in range(len(self.fingerprints[0]))]
                 else:
                     add = [np.nan for i in range(len(self.fingerprints[0]))]
                 tpatf_arr = np.array(add, dtype=np.float32)
@@ -63,7 +58,6 @@ class FeaturesGeneration:
                 pass
         
         if fp_name == 'rdkDes':
-
             X = np.array(self.fingerprints)
             ndf = pd.DataFrame.from_records(X)
             ndf.isnull().sum().sum()
@@ -77,11 +71,8 @@ class FeaturesGeneration:
             fp_array = ( np.asarray((X), dtype=object) )
             X = X.astype(np.float32)
             X = np.nan_to_num(X)
-            scalers_dir = os.path.abspath("eos8fth/model/checkpoints/scalers")
-            rdkDes_scaler_path = os.path.join(scalers_dir, model + '-rdkDes_scaler.pkl')
-            rdkDes_scaler = pickle.load(open(rdkDes_scaler_path, 'rb'))
+            rdkDes_scaler = pickle.load(open('../scalers/'+model+'-rdkDes_scaler.pkl', 'rb'))
             X = rdkDes_scaler.transform(X)
-            print("Processed Descriptors (after scaling):", X)
 
         else:
             fp_array = ( np.asarray((self.fingerprints), dtype=object) )
