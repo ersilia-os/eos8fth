b8e41cd833245fd913e110809abc0197c141a175
simrantan <96899862+simrantan@users.noreply.github.com>
2023-08-16 19:24:30 -0700

    Update run_predictions.py

--- model/framework/code/run_predictions.py
+++ model/framework/code/run_predictions.py
@@ -96,6 +96,8 @@ def get_predictions(temp_dir, results, csv_file):
             models_tuned_dir = os.path.abspath('eos8fth/model/checkpoints/models_tuned_best')
             pickle_filename = '{}-{}-balanced_randomsplit7_70_15_15.pkl'.format(fp_name, m)
             pickle_path = os.path.join(models_tuned_dir, pickle_filename)
+            print("m", m)
+            print("fp", fp)
             model = pickle.load(open(pickle_path, 'rb'))
             y_pred = model.predict(X_true)
             predictions.append(y_pred)
